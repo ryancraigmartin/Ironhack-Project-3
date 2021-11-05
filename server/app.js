34e0ed7775d376b8e5ef0a5e686b060ac9bec260
Ryan Craig Martin <ryan@ryanmartin.io>
2021-11-05 16:01:38 -0400

    add error-handling to app.js
    It's confusing to split this up. keep it in the same place for readability.

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ require('dotenv/config')
 // ℹ️ Connects to the database
 require('./db')
 
-// Handles http requests (express is node js framework)
+// Handles http requests (express is the node.js framework)
 // https://www.npmjs.com/package/express
 const express = require('express')
 
 const app = express()
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// ℹ️ This function is getting exported from the config folder. It configures most pieces of middleware to be used in the app.
 require('./config')(app)
 
 // 👇 Start handling routes here
@@ -22,7 +22,23 @@ app.use('/api/cookbooks', require('./routes/cookBook.routes.js'))
 app.use('/api/ingredients', require('./routes/ingredient.routes.js'))
 app.use('/api/recipes', require('./routes/recipe.routes.js'))
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app)
+// ❗ Routes that don't exist or errors that you handle in specific routes
+
+app.use((req, res, next) => {
+  // this middleware runs whenever requested page is not available
+  res.status(404).json({ errorMessage: 'This route does not exist' })
+})
+
+app.use((err, req, res, next) => {
+  // whenever you call next(err), this middleware will log and handle the error
+  console.error('ERROR', req.method, req.path, err)
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({
+      errorMessage: 'Internal server error. Check the server console',
+    })
+  }
+})
 
 module.exports = app
